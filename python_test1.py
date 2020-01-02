31fc1a2e9b37c09a13b35202ffa4ca745600a4e1
Isa Bostan <isabostan@gmail.com>
2020-01-02 16:38:24 +0300

    Revert "third commit"
    This reverts commit 53ba5ba7a76632faf97d9ea202406828371eb4b2.

--- python_test1.py
+++ python_test1.py
@@ -1,10 +1,10 @@
 import os
 import time
-import sys
+import datetime
 from datetime import date
+#print(dir(os))
 
 print("test1-> OS NAME: ",os.name)
 print(time.time())
 print(os.curdir)
 print(date.today())
-print(sys.version_info)
